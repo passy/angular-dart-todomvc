2b61e54bea8d5a406a17115f33b940db73da15b5
Pascal Hartig <passy@twitter.com>
2013-11-18 19:03:54 +0000

    ng-submit attempt

--- src/todo.dart
+++ src/todo.dart
@@ -1,6 +1,32 @@
 library todo;
 
+import 'dart:html' as dom;
 import 'package:angular/angular.dart';
+
+@NgDirective(
+	// ng-submit will eventually be added, using `todo-` as prefix will avoid
+	// future name clashes
+	selector: '[todo-submit]',
+	map: const {'ng-submit': '&onSubmit'}
+)
+class TodoSubmitDirective {
+	var listeners = {};
+	dom.Element element;
+	Scope scope;
+	TodoSubmitDirective(dom.Element this.element, Scope this.scope);
+	
+	set onSubmit(value) {
+		var stream = element.onSubmit;
+		int key = stream.hashCode;
+		print("registering submit handler for " + value);
+		if (!listeners.containsKey(key)) {
+			listeners[key] = value;
+			stream.listen((event) => scope.$apply(() {
+				value({r"$event": event});
+			}));
+		}
+	}
+}
 
 class Item {
 	String title;
@@ -37,5 +63,9 @@ class TodoController {
 	
 	int total() {
 		return items.length;
+	}
+	
+	String get itemsLeftText {
+		return 'item' + (remaining() != 1 ? 's' : '') + ' left';
 	}
 }
