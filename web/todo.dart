157dabf1e70d6eb252bbd3cfb9063b709c33ebe1
Pascal Hartig <passy@twitter.com>
2013-12-11 18:36:56 +0000

    Actually update the comment

--- web/todo.dart
+++ web/todo.dart
@@ -68,7 +68,9 @@ class TodoController {
 		items = storage.loadItems();
 		_storageService = storage;
 
-		// Save all items if one got inserted or removed.
+		// Since there is no native support for deeply watching collections, we
+		// instead watch over the JSON-serialized string representing the items.
+		// While hugely inefficient, this is a very simple work-around.
 		scope.$watch((Scope scope) => convert.JSON.encode(items), save);
 	}
 
